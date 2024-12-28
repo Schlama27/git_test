dd51743c3e2d438495c0ca4b50c11dc7cc0c2a5e
Kevin Thai <kschlama27@gmail.com>
2024-12-27 18:19:36 -0800

    Final product

--- Odin Project/rock-paper-scissor/rps-js-file.js
+++ Odin Project/rock-paper-scissor/rps-js-file.js
@@ -4,10 +4,36 @@ const getComputerChoice = () => {
     let randomNumber = Math.floor((Math.random() * 3) + 1);
     
     if (randomNumber === 1) {
-        return 'Rock';
+        return 'rock';
     } else if (randomNumber === 2) {
-        return 'Paper';
+        return 'paper';
     } else {
-        return 'Scissors';
+        return 'scissors';
     }
 };
+//Prompts user to type in one of the three options
+function getHumanChoice() {
+    let userInput = prompt('Choose: Rock, Paper, or Scissors?').toLowerCase();
+    return userInput;
+}
+
+console.log(getComputerChoice());
+console.log(getHumanChoice());
+
+
+//Takes both the user and computer's choice and declares a winner
+function playRound(humanChoice, computerChoice) {
+    if ((humanChoice === 'rock' && computerChoice === 'scissors') ||
+        (humanChoice === 'paper' && computerChoice === 'rock') ||
+        (humanChoice === 'scissors' && computerChoice === 'paper')) {
+            return 'humanWin';
+    } else if ((humanChoice === 'rock' && computerChoice === 'paper') ||
+                (humanChoice === 'paper' && computerChoice === 'scissors') ||
+                (humanChoice === 'scissors' && computerChoice === 'rock')) {
+                    return 'computerWin';
+                } else {
+                    return 'Tie';
+                }
+}
+
+console.log(playRound());
